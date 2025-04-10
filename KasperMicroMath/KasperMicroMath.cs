746a43219d51749bd689700f94148a2e71f62904
kaa004-zealand <kaa004@edu.zealand.dk>
2025-04-10 10:42:01 +0200

    MicroMath Add

--- KasperMicroMath/KasperMicroMath.cs
+++ KasperMicroMath/KasperMicroMath.cs
@@ -8,5 +8,15 @@ namespace KasperMicroMath
 {
     public class KasperMicroMath
     {
+        /// <summary>
+        /// Adds together 2 ints and returns the result
+        /// </summary>
+        /// <param name="a">The first int</param>
+        /// <param name="b">The second int</param>
+        /// <returns>The result of the 2 added together</returns>
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
     }
 }
